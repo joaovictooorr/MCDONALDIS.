dbbbb1397e98320e462695bb40bf29bda659adcb
arroi <joaovitorwf01020304@gmail.com>
2023-06-08 14:33:49 -0300

    atualização carnedegato

--- carnedegato.py
+++ carnedegato.py
@@ -1,103 +1,112 @@
-# if nome_cliente bug que deixa o usario passar mesmo escrevendo numeros
-# whiles não estão funcionando se escrever letras ao inves dos numeros da erro, tentar colocar uma mesangem de erro e retornar ao pedido
-# a crescentar valores aos produtos e mostrar os valores no final
-# Fazer uma função para o while (diminuir o codigo)
-# Fazer descontos vc pode criar uma variável que recebe o preço menos o desconto e fazer um if q se tiver o desconto mostra essa variável no lugar do preço
-# validar cartão de credito ou debito
+# adicionar descontos
 
 import random
 
-numero_aleatorio = random.randint(100, 999)
+numero_aleatorio = random.randint(100,999)
+
+def obter_escolha(opcoes):
+    escolha = 0
+    while escolha not in opcoes:
+        print("Escolha uma opção Válida")
+        for key, value in opcoes.items():
+            print(f"{key} - {value}")
+        escolha = int(input(" : "))
+    return escolha 
 
 def hamburguer():
-    escolha = 0
-    escolha_hambur = ""
-    while escolha == 0:
-        print("Escolha um dos hamburgueres")
-        print("1 - mc peixe")
-        print("2 - mc carne de gato")
-        print("3 - mc galinha")
-        print("4 - mc lanche feliz")
-        escolha = int(input(" : "))
-        if escolha == 1:
-            escolha_hambur = "MC PEIXE"
-        elif escolha == 2:
-            escolha_hambur = "MC CARNE DE GATO"
-        elif escolha == 3:
-            escolha_hambur = "MC GALINHA"
-        elif escolha == 4:
-            escolha_hambur = "MC LANCHE FELIZ" 
-        else:
-            print("Escolha uma opção válida")
-            escolha = 0
-
-    return escolha_hambur
-
+    opcoes = {
+        1: "MC PEIXE = R$ 15,90",
+        2: "MC CARNE DE GATO = 10,50",
+        3: "MC GALINHA = 12,40",
+        4: "MC LANCHE FELIZ = R$ 20,00"
+    } 
+    escolha = obter_escolha(opcoes)
+    return escolha
 
 def batata():
-    escolha_batata = 0
-    batatinha = ""
-    while escolha_batata == 0:
-        print("Escolha sua batata")
-        print("1 - P ")
-        print("2 - M ")
-        print("3 - G ")
-        escolha_batata = int(input(" : "))
-        if escolha_batata == 1:
-            batatinha = "PEQUENA"
-        elif escolha_batata == 2:
-            batatinha = "MEDIA"
-        elif escolha_batata == 3:
-            batatinha = "GRANDE"
-        else:
-            print("Escolha uma opção válida")
-            escolha_batata = 0
-
-    return batatinha
-
+    opcoes = {
+        1: "BATATA PEQUENA = 5,00",
+        2: "BATATA MEDIA = 8,00",
+        3: "BATATA GRANDE = 10,00"
+    }
+    escolha = obter_escolha(opcoes)
+    return escolha
 
 def refrigerante():
-    escolha_refri = 0
-    refri = ""
-    while escolha_refri == 0:
-        print("Escolha seu refrigerante")
-        print("1 - P ")
-        print("2 - M ")
-        print("3 - G ")
-        escolha_refri = int(input(" : "))
-        if escolha_refri == 1:
-            refri = "PEQUENO"
-        elif escolha_refri == 2:
-            refri = "MEDIO"
-        elif escolha_refri == 3:
-            refri = "GRANDE"
-        else:
-            print("Escolha uma opção válida")
-            escolha_refri = 0
+    opcoes = {
+        1: "REFRIGERANTE PEQUENO = 3,00",
+        2: "REFRIGERANTE MEDIO = 5,00",
+        3: "REFRIGERANTE GRANDE = 7,00"
+    }
+    escolha = obter_escolha(opcoes)
+    return escolha
 
-    return refri
+def valores(escolha_hambur, escolha_batata, escolha_refri):
+    valores_hamburguer = {
+        1: 15.90,
+        2: 10.50,
+        3: 12.40,
+        4: 20.00 
+    }
 
+    valores_batata = {
+        1: 5.0,
+        2: 8.0,
+        3: 10.0
+    }
+
+    valores_refrigerante = {
+        1: 3.0,
+        2: 5.0,
+        3: 7.0
+    }
+
+    valor_hamburguer = valores_hamburguer.get(escolha_hambur, 0)
+    valor_batata = valores_batata.get(escolha_batata, 0)
+    valor_refri = valores_refrigerante.get(escolha_refri, 0)
+
+    valor_total = valor_hamburguer + valor_batata + valor_refri
+    return valor_total
 
 def verificar_nome():
     nome_cliente = ""
     while not nome_cliente:
-        nome_cliente = input("Digite o nome do Cliente: ")
+        nome_cliente = input("Digite o nome do cliente: ")
         if nome_cliente.isnumeric():
-            print("Erro: Você digitou um número.")
+            print("Apenas são aceitos letras e acentuações")
             nome_cliente = ""
     return nome_cliente
 
+def nota_fiscal(valor_escolha_hamburguer, valor_escolha_batata, valor_escolha_refrigerante, nome_cliente, valor_total):
+    opcoes_hamburguer = {
+        1: "MC PEIXE - R$ 15,90",
+        2: "MC CARNE DE GATO - R$ 10,50",
+        3: "MC GALINHA - R$ 12,40",
+        4: "MC LANCHE FELIZ - R$ 20,00"
+    }
 
-def nota_fiscal(escolha_hambur, batatinha, refri, nome_cliente):
-    print(f" NÚMERO DO PEDIDO: {numero_aleatorio} ")
+    opcoes_batata = {
+        1: " BATATA PEQUENA - R$ 5,00",
+        2: " BATATA MEDIA - R$ 8,00",
+        3: " BATATA GRANDE - R$ 10,00"        
+    }
+
+    opcoes_refri = {
+        1: " REFRIGERANTE PEQUENO - R$ 3,00",
+        2: " REFRIGERANTE MEDIO - R$ 5,00",
+        3: " REFRIGERANTE GRANDE - R$ 7,00"
+    }
+
+    print(f" NÚMERO DO PEDIDO: {numero_aleatorio}")
     print(f" CLIENTE: {nome_cliente}")
-    print(f" HAMBURGUER: {escolha_hambur}")
-    print(f" TAMANHO DA BATATA: {batatinha}")
-    print(f" TAMANHO DO REFRIGERANTE: {refri}")
+    print(f" HAMBURGUER: {opcoes_hamburguer[valor_escolha_hamburguer]}")
+    print(f" BATATA: {opcoes_batata[valor_escolha_batata]}")
+    print(f" REFRIGERANTE: {opcoes_refri[valor_escolha_refrigerante]} ")
+    print(f" VALOR TOTAL DA COMPRA: R$ {valor_total:.2f}")
 
-
-escolha_hambur = hamburguer()
-batatinha = batata()
-refri = refrigerante()
+valor_escolha_hamburguer = hamburguer()
+valor_escolha_batata = batata()
+valor_escolha_refrigerante = refrigerante()
+valor_total = valores(valor_escolha_hamburguer,valor_escolha_batata,valor_escolha_refrigerante)
 nome_cliente = verificar_nome()
-nota_fiscal(escolha_hambur, batatinha, refri, nome_cliente)
+nota_fiscal (valor_escolha_hamburguer,valor_escolha_batata,valor_escolha_refrigerante, nome_cliente, valor_total)
